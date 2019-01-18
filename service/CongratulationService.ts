9e92cfd83461b51bfceed93a1881e89ac629a473
Sauli Ketola <sauli.ketola@nitor.fi>
2019-01-18 13:56:47 +0200

    Remove additional spaces from message

--- service/CongratulationService.ts
+++ service/CongratulationService.ts
@@ -28,7 +28,7 @@ class CongratulationService {
     const employee = employees[nthWeekday];
     const yearsAtCompany: number = this.yearsPresent(employee, date);
     const message: string = `Congratulations **${employee.fullName}** ` +
-      `${yearsAtCompany} ${(yearsAtCompany === 1 ? " year" : " years")}  at Nitor! :tada:`;
+      `${yearsAtCompany} ${(yearsAtCompany === 1 ? "year" : "years")} at Nitor! :tada:`;
     await this.flowdockService.sendMessage(message);
     console.log(message);
 
